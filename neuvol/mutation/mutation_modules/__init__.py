d1a18d512781fb65ed5f609a7dba0bbe749d0cb3
Unknown <qwinpin@gmail.com>
2018-07-27 17:08:07 +0300

    Auto stash before revert of "Mutation class refactoring"

--- neuvol/mutation/mutation_modules/__init__.py
+++ neuvol/mutation/mutation_modules/__init__.py
@@ -11,5 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from mutation_modules_interface import perform_mutation
+from .mutation_modules_interface import perform_mutation
 
